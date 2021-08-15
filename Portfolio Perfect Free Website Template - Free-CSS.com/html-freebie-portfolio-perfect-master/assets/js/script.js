972b557f3c3c7923596a94476a3fc7e1e11e4638
pkh2351 <76101223+fkdla1963@users.noreply.github.com>
2021-08-16 04:40:49 +0900

    2021-08-16

--- Portfolio Perfect Free Website Template - Free-CSS.com/html-freebie-portfolio-perfect-master/assets/js/script.js
+++ Portfolio Perfect Free Website Template - Free-CSS.com/html-freebie-portfolio-perfect-master/assets/js/script.js
@@ -87,3 +87,11 @@ $(function() {
     });
     
 });
+
+// my custom
+const setDirection = document.getElementById("writing-mode");
+const content = document.getElementById("page-content");
+
+setDirection.addEventListener("change", (e) => {
+  content.setAttribute("dir", e.target.value);
+});
